b5f310412e32967afe0c92277db139750fc7c888
nbbryy <90761308+nbbryy@users.noreply.github.com>
2022-03-06 23:49:53 +0800

    Add files via upload

---
+++ Lecture 1/Assignments/assignment_lec1_c1.c
@@ -0,0 +1,21 @@
+//Lecture 1 Assignment
+//No. 1
+
+#include <stdio.h>
+
+int main(void){
+
+    //first line (a)
+    printf("a. In C, lowercase letters are significant.\n");
+
+    //second line (b)
+    printf("b. main is where program execute begins.\n");
+
+    //third line (c)
+    printf("c. Opening and closing braces enclose program statements in a routine.\n");
+
+    //fourth line (d)
+    printf("d. All program statements must be terminated by a semicolon.\n");
+
+    return 0;
+}
---
+++ Lecture 1/Assignments/assignment_lec1_c2.c
@@ -0,0 +1,18 @@
+//Lecture 1 Assignment
+//No.3
+
+#include <stdio.h>
+
+int main(void){
+
+    int first_num, second_num, result;  //declare variables in int types
+
+    first_num = 15;     //assign values to variables
+    second_num = 87;
+
+    result = second_num - first_num;    //compute result
+
+    printf("If we subtract 15 from 87, the result will be %d.\n", result);  //display result
+
+    return 0;
+}
